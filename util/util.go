4610819cc4763a6da2d1ceac4d48cc500e27961c
Dan Lugg <dan.lugg@gmail.com>
2025-08-13 16:24:15 -0400

    remove dead code from util #patch

--- util/util.go
+++ util/util.go
@@ -2,14 +2,17 @@ package util
 
 type Option[TAny any] func(TAny)
 
+type Tuple1[T1 any] struct {
+	Val1 T1
+}
+
 type Tuple2[T1 any, T2 any] struct {
 	Val1 T1
 	Val2 T2
 }
 
-func NewTuple2[T1 any, T2 any](val1 T1, val2 T2) Tuple2[T1, T2] {
-	return Tuple2[T1, T2]{
-		Val1: val1,
-		Val2: val2,
-	}
+type Tuple3[T1 any, T2 any, T3 any] struct {
+	Val1 T1
+	Val2 T2
+	Val3 T3
 }
